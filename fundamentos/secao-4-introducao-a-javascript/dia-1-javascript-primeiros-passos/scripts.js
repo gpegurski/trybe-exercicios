65c5a63b42fad053404ac17f1e5d57c146dbd02f
gpegurski <grazipegurski@gmail.com>
2022-10-20 20:53:02 -0300

    criacao if com || e ===

--- fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/scripts.js
@@ -21,7 +21,7 @@ const perimeter = (base * 2) + (heigth * 2);
 
 //console.log(perimeter);
 
-const nota = 80;
+/*const nota = 80;
 
 if(nota >= 80) {
     console.log('Parabéns, você foi aprovado(a)!');
@@ -31,4 +31,33 @@ else if(nota < 80 && nota >= 60) {
 }
 else {
     console.log('Você foi reprovado(a)')
+}*/
+
+const currentHour = 8;
+let message = '';
+
+if(currentHour >= 22) {
+    message = 'Não deveríamos comer nada, é hora de dormir';
+}
+else if (currentHour >= 18 && currentHour < 22) {
+    message = 'Rango da noite, vamos jantar :D';
+}
+else if (currentHour >= 14 && currentHour < 18) {
+    message = 'Vamos fazer um bolo pro café da tarde?';
+}
+else if (currentHour >= 11 && currentHour < 14) {
+    message = 'Hora do almoço!!!';
+}
+else if (currentHour >= 4 && currentHour < 11) {
+    message = 'Hmmm, cheiro de café recém-passado';
+}
+//console.log(message);
+
+let weekDay = 'quarta-feira';
+
+if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+    console.log('Oba, mais um dia de aprendizado na Trybe :D')
+}
+else {
+    console.log('FINALMENTE, descanso merecido!');
 }
