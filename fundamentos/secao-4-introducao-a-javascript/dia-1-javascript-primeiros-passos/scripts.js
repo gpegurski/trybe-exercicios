205865cb9c92fbb537aa6c1d450c650edf18deed
gpegurski <grazipegurski@gmail.com>
2022-10-20 20:06:21 -0300

    criacao validacao nota da prova

--- fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/scripts.js
@@ -15,8 +15,20 @@ const base = 5;
 const heigth = 8;
 const area = base * heigth;
 
-console.log(area);
+//console.log(area);
 
 const perimeter = (base * 2) + (heigth * 2);
 
-console.log(perimeter);
+//console.log(perimeter);
+
+const nota = 80;
+
+if(nota >= 80) {
+    console.log('Parabéns, você foi aprovado(a)!');
+} 
+else if(nota < 80 && nota >= 60) {
+    console.log('Você está na nossa lista de espera!')
+}
+else {
+    console.log('Você foi reprovado(a)')
+}
